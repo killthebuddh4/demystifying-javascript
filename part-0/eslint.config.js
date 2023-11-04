a0937c7a5630d4cae4096fafe1499198ec8517ce
achilles schmelzer <achilles@daopanel.com>
2023-11-04 12:14:05 -0700

    Add mocha/node envs

--- part-0/eslint.config.js
+++ part-0/eslint.config.js
@@ -1,6 +1,22 @@
+import globals from 'globals';
 import js from "@eslint/js";
 
 export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: [ "**/*.test.js" ],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
   js.configs.recommended,
 ];
 
